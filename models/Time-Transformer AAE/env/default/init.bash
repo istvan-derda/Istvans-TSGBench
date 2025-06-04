d700035e342e5590418c5ecbec717d7eb7685e9f
István Derda <istvan.derda@mailbox.org>
2025-06-04 11:50:13 +0000

    Rename TimeTransformer to Time-Transformer AAE

--- models/Time-Transformer AAE/env/default/init.bash
+++ models/Time-Transformer AAE/env/default/init.bash
@@ -1,4 +1,4 @@
-[[ ! $PWD == */models/TimeTransformer ]] && echo "Error: Please source this from models/TimeTransformer with the command 'source env/default/init'" && return
+[[ ! $PWD == */models/Time-Transformer\ AAE ]] && echo "Error: Please source this from models/Time-Transformer AAE with the command 'source env/sc_uni_leipzig/init" && return
 
 git clone https://github.com/Lysarthas/Time-Transformer.git Time_Transformer
 (cd Time_Transformer && git checkout ff8faa27353e721702aaa51136192f071e5a5e19)
