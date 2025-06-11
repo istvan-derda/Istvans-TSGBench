584a869e7634b7321be65824c9eea50cb5d0e5ac
István Derda <istvan.derda@mailbox.org>
2025-06-11 11:43:41 +0200

    Time-Transformer AAE: Fix slurm script and configure notifictaitons

--- models/Time-Transformer AAE/slurmfile.bash
+++ models/Time-Transformer AAE/slurmfile.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
 #SBATCH -J Time-Transformer-AAE
-#SBATCH --time=03:00
-#SBATCH --nodes=6
-#SBATCH --ntasks-per-node=1
+#SBATCH --time=03:00:00
+#SBATCH --array=2-7
+#SBATCH --partition=clara
 #SBATCH --gpus=v100
-#SBATCH --mem=2G
-#SBATCH -o $HOME/time-transformer.out-%a-%A
-#SBATCH -e $HOME/time-transformer.err-%a-%A
+#SBATCH --mem=4G
+#SBATCH -o jobfiles/log/%x.out-%j
 
-DATASET_NO=$(( $SLURM_ARRAY_TASK_ID + 1 ))
+DATASET_NO=$SLURM_ARRAY_TASK_ID
 
+curl -d "Started Time-Transformer AAE on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
+
+source env/sc_uni_leipzig/init.bash
 python run_time_transformer.py --dataset_no $DATASET_NO
+
+curl -d "Finished Time-Transformer AAE on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
