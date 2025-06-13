9a0946669748d35990140ed4b0a924e097b9064f
István Derda <istvan.derda@mailbox.org>
2025-06-13 13:45:54 +0200

    TTS-GAN: Environment reliably working on uni leipzig hpc

--- models/TTS-GAN/env/sc_uni_leipzig/setup_env.sh
+++
@@ -1,8 +0,0 @@
-module purge 
-
-python -m venv env/sc_uni_leipzig/venv
-source env/sc_uni_leipzig/venv/bin/activate
-pip install -r requirements.txt
-
-module load torchvision
-module load TensorFlow
