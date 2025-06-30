a5ad1e1800d66644b0ca152b9ee47ebf624a6472
István Derda <istvan.derda@mailbox.org>
2025-06-30 11:57:07 +0200

    Time-Transformer AAE: Use pip instead of conda
    Use pip instead of conda on sc infrastructure of leipzig university for
    conda interacting weirdly with module load commands.

--- models/Time-Transformer AAE/env/sc_uni_leipzig/init.bash
+++
@@ -1,9 +0,0 @@
-[[ ! $PWD == */models/Time-Transformer\ AAE ]] && echo "Error: Please source this from models/Time-Transformer AAE with the command 'source env/sc_uni_leipzig/init" && return
-
-git clone https://github.com/Lysarthas/Time-Transformer.git Time_Transformer
-(cd Time_Transformer && git checkout ff8faa27353e721702aaa51136192f071e5a5e19)
-
-module purge
-conda env update -f env/sc_uni_leipzig/environment.yml --prune
-conda activate time-transformer
-module load TensorFlow
--- models/Time-Transformer AAE/slurmfile.bash
+++ models/Time-Transformer AAE/slurmfile.bash
@@ -4,14 +4,14 @@
 #SBATCH --array=2-7
 #SBATCH --partition=clara
 #SBATCH --gpus=v100
-#SBATCH --mem=4G
+#SBATCH --mem=8G
 #SBATCH -o jobfiles/log/%x.out-%j
 
 DATASET_NO=$SLURM_ARRAY_TASK_ID
 
 curl -d "Started Time-Transformer AAE on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
 
-source env/sc_uni_leipzig/init.bash
+source env/sc_uni_leipzig/setup_env
 python run_time_transformer.py --dataset_no $DATASET_NO
 
 curl -d "Finished Time-Transformer AAE on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
