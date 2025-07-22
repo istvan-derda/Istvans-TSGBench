e1ad63b6a85855ac6fb292004271c1323cdff8b1
István Derda <istvan.derda@mailbox.org>
2025-07-22 11:02:02 +0000

    TransFusion: runs on my machine

---
+++ models/TransFusion/pull-TransFusion.sh
@@ -0,0 +1,3 @@
+rm -rf TransFusion
+git clone git@github.com:istvan-derda/Istvans-TransFusion.git TransFusion
+$(cd TransFusion && git checkout e0f0a62d5a39c45b6d8a1a37b7d163b79194d390)
---
+++ models/TransFusion/run_transfusion.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import numpy as np
+
+sys.path.append('../') # make tsgb_utils import work
+sys.path.append(os.path.join(os.path.dirname(__file__), 'TransFusion')) # make imports in TransFusion/train.py work
+
+from tsgb_utils import load_train_data, persist_gen_data
+from TransFusion.train import train, generate
+
+
+def main():
+    # Load Data
+    train_data = load_train_data().astype(np.float32)
+    seq_len = train_data.shape[1]
+
+    # Train Model
+    model = train(train_data=train_data, seq_len=seq_len)
+
+    # Generate Synthetic Data
+    gen = generate(model=model, len=seq_len)
+
+    # Persist Synthetic Data
+    persist_gen_data(gen)
+
+
+if __name__ == '__main__':
+    main()
