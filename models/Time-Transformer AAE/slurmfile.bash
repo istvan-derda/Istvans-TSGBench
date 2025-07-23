29fb4000f5cda471b08508f372a3784140c98aea
István Derda <istvan.derda@mailbox.org>
2025-07-23 09:21:12 +0200

    Time-Transformer AAE: move env setup logic into slurmfile

--- models/Time-Transformer AAE/slurmfile.bash
+++ models/Time-Transformer AAE/slurmfile.bash
@@ -8,12 +8,36 @@
 #SBATCH -o jobfiles/%x_%A_%a.out
 #SBATCH -e jobfiles/%x_%A_%a.err
 
+log_stdout_stderr() {
+    echo "$@" | tee >(cat >&2)
+}
+
+log() {
+    local msg="[$(date +'%Y-%m-%d %H:%M:%S')] [NOTICE] $*"
+    local border
+    border=$(printf '%*s' "${COLUMNS:-80}" '' | tr ' ' '#')
+
+    log_stdout_stderr "$border"
+    log_stdout_stderr "$msg"
+    log_stdout_stderr "$border"
+}
 
 DATASET_NO=$SLURM_ARRAY_TASK_ID
 
-curl -d "Started Time-Transformer AAE on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
+curl -d "Started Time-Transformer AAE job on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
+log "Started Time-Transformer AAE job on Dataset D$DATASET_NO (Job ID: $SLURM_JOB_ID)"
 
-source env/sc_uni_leipzig/setup_env
+log "Resetting environment"
+module purge
+pip freeze --user | xargs pip uninstall -y
+
+log "Setting up environment"
+module load TensorFlow
+pip install numpy keras==2.11.0 scikit-learn==1.0.2 scipy==1.7.3 mgzip==0.2.1
+
+log "Starting Training Script"
 python run_time_transformer.py --dataset_no $DATASET_NO
+log "Training Script Terminated"
 
 curl -d "Finished Time-Transformer AAE on Dataset D$DATASET_NO" ntfy.sh/istvanshpcunileipzig
+log "Finished Time-Transformer AAE on Dataset D$DATASET_NO"
