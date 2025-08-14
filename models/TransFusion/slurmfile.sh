b5794ef1af3693ae29d5ec3cf3cd1d47a4174f12
István Derda <istvan.derda@mailbox.org>
2025-08-14 13:09:57 +0200

    TransFusion: prevent pip installation conflicts between jobs with venvs

--- models/TransFusion/slurmfile.sh
+++ models/TransFusion/slurmfile.sh
@@ -27,17 +27,20 @@ DATASET_NO=$SLURM_ARRAY_TASK_ID
 curl -d "Started TransFusion job on Dataset D$DATASET_NO (Job ID: $SLURM_JOB_ID)" ntfy.sh/istvanshpcunileipzig
 log_progress "Started TransFusion job on Dataset D$DATASET_NO (Job ID: $SLURM_JOB_ID)"
 
-log_progress "Resetting environment"
-module purge
-pip freeze --user | xargs pip uninstall -y
-
 log_progress "Setting up environment"
+module purge
 module load PyTorch/1.12.1-foss-2021b-CUDA-11.5.2 
+VENV_DIR=/tmp/slurm_job_${SLURM_JOB_ID}_venv
+python -m venv --system-site-packages $VENV_DIR
+source $VENV_DIR/bin/activate
 pip install pandas scikit-learn tqdm scipy einops tensorboard numba pyprojroot mgzip
 
 log_progress "Starting Training Script"
 python run_transfusion.py --dataset_no=$DATASET_NO
 log_progress "Training Script Terminated"
+
+deactivate
+module purge
 
 curl -d "Finished TransFusion job on Dataset D$DATASET_NO (Job ID: $SLURM_JOB_ID)" ntfy.sh/istvanshpcunileipzig
 log_progress "Finished TransFusion job on Dataset D$DATASET_NO (Job ID: $SLURM_JOB_ID)"
