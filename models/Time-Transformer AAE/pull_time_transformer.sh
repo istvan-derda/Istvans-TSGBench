7ccb8be73aa169822d0a0c9ab3ee0f7833962580
István Derda <istvan.derda@mailbox.org>
2025-08-14 12:57:28 +0200

    Time-Transformer AAE: cleanup: move pulling model repository to justfile

--- models/Time-Transformer AAE/pull_time_transformer.sh
+++
@@ -1,6 +0,0 @@
-rm -rf Time_Transformer
-
-git clone https://github.com/Lysarthas/Time-Transformer.git Time_Transformer
-(cd Time_Transformer && git checkout ff8faa27353e721702aaa51136192f071e5a5e19)
-
-
