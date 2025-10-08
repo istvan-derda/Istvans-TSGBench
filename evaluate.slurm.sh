0208a08200255f524e138dbb006f101cc0b8f2b2
István Derda <istvan.derda@mailbox.org>
2025-10-08 14:52:44 +0200

    TimeVQVAE: evaluation result

--- evaluate.slurm.sh
+++ evaluate.slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -J TSGBench_Evaluation
 #SBATCH --time=2-00:00:00
-#SBATCH --array=0-4
+#SBATCH --array=0-5
 #SBATCH --partition=clara
 #SBATCH --gpus=rtx2080ti
 #SBATCH --mem=8G
 #SBATCH -o jobfiles/%x_%A_%a.out
 #SBATCH -e jobfiles/%x_%A_%a.err
 
-methods=("JustCopy" "TimeGAN" "TTS-GAN" "Time-Transformer" "TransFusion")
+methods=("JustCopy" "TimeGAN" "TTS-GAN" "Time-Transformer" "TransFusion" "TimeVQVAE")
 
 method=${methods[$SLURM_ARRAY_TASK_ID]}
 
